ba605f7dde646ebeabd54de2bf9130b7678b8528
Greg Back <gback@mitre.org>
2017-07-20 18:10:07 +0000

    WIP: Reshaping TAXII Client

--- taxii2_client/__init__.py
+++ taxii2_client/__init__.py
@@ -1,6 +1,6 @@
-import requests
-from requests.auth import HTTPBasicAuth
 import os
+
+import requests
 
 os.environ["NO_PROXY"] = "localhost"
 
@@ -8,6 +8,7 @@ MEDIA_TYPE_STIX_V20 = "application/vnd.oasis.stix+json; version=2.0"
 MEDIA_TYPE_TAXII_V20 = "application/vnd.oasis.taxii+json; version=2.0"
 
 
+# TODO: Should this object allow refreshing itself (have its own URL?)
 class Status(object):
     def __init__(self, id, status, total_count, success_count, failure_count, pending_count, request_timestamp=None,
                  successes=None, failures=None, pendings=None):
@@ -20,144 +21,267 @@ class Status(object):
 
 
 class Collection(object):
-    def __init__(self, api_root, id, media_types, title=None, can_write=True, can_read=True, description=None):
-        self.id_ = id
-        self.api_root = api_root
-        self.taxii_client = api_root.taxii_client
-        self.media_types = media_types
-        self.title = title
-        self.can_write = can_write
-        self.can_read = can_read
-        self.description = description
+    def __init__(self, url, client=None, **kwargs):
+        self.url = url
 
+        self._client = client
+        self._loaded = False
+
+        # Since the API Root "Get Collections" endpoint returns information on
+        # all collections as a list, it's possible that we can create multiple
+        # Collection objects from a single HTTPS request, and not need to call
+        # `refresh` for each one.
+        if kwargs:
+            self._populate_fields(**kwargs)
+            self._loaded = True
+
+    @property
+    def id(self):
+        self._ensure_loaded()
+        return self._id
+
+    @property
+    def title(self):
+        self._ensure_loaded()
+        return self._title
+
+    @property
+    def description(self):
+        self._ensure_loaded()
+        return self._description
+
+    @property
+    def can_read(self):
+        self._ensure_loaded()
+        return self._can_read
+
+    @property
+    def can_write(self):
+        self._ensure_loaded()
+        return self._can_write
+
+    @property
+    def media_types(self):
+        self._ensure_loaded()
+        return self._media_types
+
+    def _populate_fields(self, **kwargs):
+        self._id = kwargs.pop('id', None)
+        # TODO: ensure id doesn't change (or at least matches self.url)
+        self._title = kwargs.pop('title', None)
+        self._description = kwargs.pop('description', None)
+        self._can_read = kwargs.pop('can_read', False)
+        self._can_write = kwargs.pop('can_write', False)
+        self._media_types = kwargs.pop('media_types', [])
+
+        # TODO: Check for unexpected kwargs
+
+    def _ensure_loaded(self):
+        if not self._loaded:
+            self.refresh()
+
+    def refresh(self):
+        response = self._client.get(self.url)
+        self._populate_fields(**response)
+
+        self._loaded = True
+
+    # TODO: update this function
     def get_object(self, obj_id):
         if not self.can_read:
             raise ValueError("Collection %s of %s does not allow reading" % (self.id_, self.api_root.uri))
         return self.taxii_client.send_request("get",
-                                              "/".join([self.api_root.uri, "collections", self.id_, "objects", obj_id]),
+                                              "/".join([self.api_root.url, "collections", self.id_, "objects", obj_id]),
                                               {"Accept": MEDIA_TYPE_STIX_V20})
 
+    # TODO: update this function
     def get_objects(self, filters=None):
         if not self.can_read:
             raise ValueError("Collection %s of %s does not allow reading" % (self.id_, self.api_root.uri))
         return self.taxii_client.send_request("get",
-                                              "/".join([self.api_root.uri, "collections", self.id_, "objects"]),
+                                              "/".join([self.api_root.url, "collections", self.id_, "objects"]),
                                               {"Accept": MEDIA_TYPE_STIX_V20},
                                               params=filters)
 
-
+    # TODO: update this function
     def add_objects(self, bundle):
         if not self.can_write:
             raise ValueError("Collection %s of %s does not allow writing" % (self.id_, self.api_root.uri))
         info = self.taxii_client.send_request("post",
-                                              "/".join([self.api_root.uri, "collections", self.id_, "objects"])+"/",
+                                              "/".join([self.api_root.url, "collections", self.id_, "objects"])+"/",
                                               {"Accept": MEDIA_TYPE_TAXII_V20,
                                                "Content-Type": MEDIA_TYPE_STIX_V20},
                                               json=bundle)
         return Status(**info)
 
+    # TODO: update this function
     def get_manifest(self, filters=None):
         return self.taxii_client.send_request("get",
-                                              "/".join([self.api_root.uri, "collections", self.id_, "manifest"]),
+                                              "/".join([self.api_root.url, "collections", self.id_, "manifest"]),
                                               {"Accept": MEDIA_TYPE_TAXII_V20})
+
 
 class ApiRoot(object):
 
-    def __init__(self, uri, taxii_client, name, collections=None, status=None, information=None):
-        self.uri = uri
-        self.taxii_client = taxii_client
-        self.name = name
-        self.collections = self.get_collections(True)
-        self.information = self.get_information(True)
+    def __init__(self, url, client=None):
+        self.url = url
 
-    def get_collections(self, refresh=False):
-        if refresh or not self.collections:
-            info = self.taxii_client.send_request("get",
-                                                  self.uri + "/" + "collections",
-                                                  {"Accept": MEDIA_TYPE_TAXII_V20})
-            self.collections = []
-            for collection in info["collections"]:
-                c = Collection(self, **collection)
-                self.collections.append(c)
-        return self.collections
+        self._client = client
+        self._loaded_collections = False
+        self._loaded_information = False
 
-    def get_information(self, refresh=False):
-        if refresh or not self.information:
-            self.information = self.taxii_client.send_request("get",
-                                                              self.uri,
-                                                              {"Accept": MEDIA_TYPE_TAXII_V20})
-        return self.information
+        # TODO: Consider adding a `name` @property: the last URL path component
+        # self.name = name
 
+    @property
+    def collections(self):
+        if not self._loaded_collections:
+            self.refresh_collections()
+        return self._collections
+
+    @property
+    def title(self):
+        self._ensure_loaded_information()
+        return self._title
+
+    @property
+    def description(self):
+        self._ensure_loaded_information()
+        return self._description
+
+    @property
+    def versions(self):
+        self._ensure_loaded_information()
+        return self._versions
+
+    @property
+    def max_content_length(self):
+        self._ensure_loaded_information()
+        return self._max_content_length
+
+    def _ensure_loaded_information(self):
+        if not self._loaded_information:
+            self.refresh_information()
+
+    def refresh():
+        self.refresh_information()
+        self.refresh_collections()
+
+    def refresh_information(self):
+        response = self._client.get(self.url)
+
+        self._title = response['title']
+        self._description = response['description']
+        self._versions = response['versions']
+        self._max_content_length = response['max_content_length']
+
+        self._loaded_information = True
+
+    def refresh_collections(self):
+        url = self.url + 'collections/'
+        response = self._client.get(url)  # {"Accept": MEDIA_TYPE_TAXII_V20})
+
+        self._collections = []
+        print(response)
+        for item in response['collections']:
+            collection_url = url + item['id'] + "/"
+            collection = Collection(collection_url, client=self._client, **item)
+            self._collections.append(collection)
+
+        self._loaded_collections = True
+
+    # TODO: update this function
     def get_collection(self, id_, refresh=False):
         for c in self.get_collections(refresh):
             if c.id_ == id_:
                 return c
 
+    # TODO: update this function
     def get_status(self, id_):
         info = self.taxii_client.send_request("get",
-                                              "/".join([self.uri, "status", id_]),
+                                              "/".join([self.url, "status", id_]),
                                               {"Accept": MEDIA_TYPE_TAXII_V20})
         return Status(**info)
 
 
+class ServerInfo(object):
+
+    def __init__(self, hostname, client=None):
+        self.hostname = hostname
+
+        self._client = client
+        self._loaded = False
+
+    @property
+    def title(self):
+        self._ensure_loaded()
+        return self._title
+
+    @property
+    def description(self):
+        self._ensure_loaded()
+        return self._description
+
+    @property
+    def contact(self):
+        self._ensure_loaded()
+        return self._contact
+
+    @property
+    def default(self):
+        self._ensure_loaded()
+        return self._default
+
+    @property
+    def api_roots(self):
+        self._ensure_loaded()
+        return self._api_roots
+
+    def _ensure_loaded(self):
+        if not self._loaded:
+            self.refresh()
+
+    def refresh(self):
+        # TODO: ensure client exists
+        url = "https://{}/taxii/".format(self.hostname)
+        response = self._client.get(url)  # {"Accept": MEDIA_TYPE_TAXII_V20})
+
+        self._title = response['title']
+        self._description = response['description']
+        self._contact = response['contact']
+        # TODO: if same URL is in api_roots, re-use the same object
+        self._default = ApiRoot(response['default'], client=self._client)
+        self._api_roots = [ApiRoot(url, client=self._client)
+                           for url in response['api_roots']]
+
+        self._loaded = True
+
+
 class TAXII2Client(object):
 
-    def __init__(self, server_uri, user, password):
-        self.server_uri = server_uri
-        self.api_roots = []
-        self.server_title = None
-        self.server_description = None
-        self.server_contact = None
-        self.default_api_root = None
-        self.auth_info = HTTPBasicAuth(user, password)
+    def __init__(self, user, password):
+        self.session = requests.Session()
+        self.session.auth = requests.auth.HTTPBasicAuth(user, password)
 
-    def populate_available_information(self):
-        info = self.send_request("get",
-                                 self.server_uri + "/" + "taxii",
-                                 {"Accept": MEDIA_TYPE_TAXII_V20})
-        for api_root in info["api_roots"]:
-            ar = ApiRoot(self.canonicalize_api_root_uri(api_root), self, self.get_api_root_name(api_root))
-            self.api_roots.append(ar)
-        self.server_title = info["title"]
-        self.server_description = info["description"]
-        if info["default"]:
-            self.default_api_root = self.get_api_root(info["default"])
-
-    def refresh_available_information(self):
-        pass
-
-    def get_api_root(self, name):
-        for a_r in self.api_roots:
-            if a_r.name == name:
-                return a_r
-
-    def send_request(self, request_type, url, headers, json=None, params=None):
-        # could be fancier to make the call, but not sure its worth it
-        if request_type == "get":
-            resp = requests.get(url, headers=headers, params=params, auth=self.auth_info)
-        elif request_type == "post":
-            resp = requests.post(url, headers=headers, params=params, json=json, auth=self.auth_info)
-        else:
-            raise ValueError("request type %s not supported" % request_type)
+    def get(self, url, headers=None, params=None):
+        resp = self.session.get(url, headers=headers, params=params)
         resp.raise_for_status()
         return resp.json()
 
-    #def get_collections(self, api_root, refresh=False):
-    #    return api_root.get_collection(refresh)
-
-    @staticmethod
-    def canonicalize_api_root_uri(api_root_uri):
-        if api_root_uri.endswith("/"):
-            api_root_uri = api_root_uri[:-1]
-        return api_root_uri
+    def post(self, url, headers=None, param=None, json=None):
+        resp = self.session.post(url, headers=headers, params=params, json=json)
+        resp.raise_for_status()
+        return resp.json()
 
 
-    @staticmethod
-    def get_api_root_name(api_root_uri):
-        if api_root_uri.endswith("/"):
-            api_root_uri = api_root_uri[:-1]
-        return api_root_uri.split('/')[-1]
+# TODO: are these needed?
+def canonicalize_api_root_url(api_root_url):
+    if api_root_url.endswith("/"):
+        api_root_url = api_root_url[:-1]
+    return api_root_url
 
 
-
-
+def get_api_root_name(api_root_url):
+    if api_root_url.endswith("/"):
+        api_root_url = api_root_url[:-1]
+    return api_root_url.split('/')[-1]
---
+++ taxii2_client/test/test_client.py
@@ -0,0 +1,150 @@
+import pytest
+import responses
+import requests
+
+from taxii2_client import (Collection, ApiRoot, ServerInfo, TAXII2Client,
+                           MEDIA_TYPE_STIX_V20, MEDIA_TYPE_TAXII_V20)
+
+TAXII_SERVER = 'example.com'
+DISCOVERY_URL = 'https://{}/taxii/'.format(TAXII_SERVER)
+API_ROOT_URL = 'https://{}/api1/'.format(TAXII_SERVER)
+COLLECTIONS_URL = API_ROOT_URL + 'collections/'
+COLLECTION_URL = COLLECTIONS_URL + '91a7b528-80eb-42ed-a74d-c6fbd5a26116/'
+
+
+DISCOVERY_RESPONSE = """{
+  "title": "Some TAXII Server",
+  "description": "This TAXII Server contains a listing of...",
+  "contact": "string containing contact information",
+  "default": "https://example.com/api2/",
+  "api_roots": [
+    "https://example.com/api1/",
+    "https://example.com/api2/",
+    "https://example.net/trustgroup1/"
+  ]
+}"""
+API_ROOT_RESPONSE = """{
+  "title": "Malware Research Group",
+  "description": "A trust group setup for malware researchers",
+  "versions": ["taxii-2.0"],
+  "max_content_length": 9765625
+}"""
+COLLECTIONS_RESPONSE = """{
+  "collections": [
+    {
+      "id": "91a7b528-80eb-42ed-a74d-c6fbd5a26116",
+      "title": "High Value Indicator Collection",
+      "description": "This data collection is for collecting high value IOCs",
+      "can_read": true,
+      "can_write": false,
+      "media_types": [
+        "application/vnd.oasis.stix+json; version=2.0"
+      ]
+    },
+    {
+      "id": "52892447-4d7e-4f70-b94d-d7f22742ff63",
+      "title": "Indicators from the past 24-hours",
+      "description": "This data collection is for collecting current IOCs",
+      "can_read": true,
+      "can_write": false,
+      "media_types": [
+        "application/vnd.oasis.stix+json; version=2.0"
+      ]
+    }
+  ]
+}"""
+COLLECTION_RESPONSE = """{
+  "id": "91a7b528-80eb-42ed-a74d-c6fbd5a26116",
+  "title": "High Value Indicator Collection",
+  "description": "This data collection is for collecting high value IOCs",
+  "can_read": true,
+  "can_write": false,
+  "media_types": [
+    "application/vnd.oasis.stix+json; version=2.0"
+  ]
+}"""
+
+
+@pytest.fixture
+def client():
+    return TAXII2Client(None, None)
+
+
+@responses.activate
+def test_server(client):
+    responses.add(responses.GET, DISCOVERY_URL, body=DISCOVERY_RESPONSE,
+                  status=200, content_type='application/json')
+    server = ServerInfo('example.com', client=client)
+
+    assert server._loaded is False
+    assert server.title == "Some TAXII Server"
+    assert server._loaded is True
+    assert server.description == "This TAXII Server contains a listing of..."
+    assert server.contact == "string containing contact information"
+    assert len(server.api_roots) == 3
+    assert server.default is not None
+
+    # TODO: make this work
+    # assert server.api_roots[1] == server.default
+
+    api_root = server.api_roots[0]
+    assert api_root.url == API_ROOT_URL
+    # The URL is populated based on the discovery response, so the rest of the
+    # information is not loaded yet.
+    assert api_root._loaded_information is False
+
+
+@responses.activate
+def test_api_root(client):
+    responses.add(responses.GET, API_ROOT_URL, API_ROOT_RESPONSE,
+                  status=200, content_type='application/json')
+
+    api_root = ApiRoot(API_ROOT_URL, client=client)
+
+    assert api_root._loaded_information is False
+    assert api_root.title == "Malware Research Group"
+    assert api_root._loaded_information is True
+    assert api_root.description == "A trust group setup for malware researchers"
+    assert api_root.versions == ['taxii-2.0']
+    assert api_root.max_content_length == 9765625
+
+
+@responses.activate
+def test_api_root_collections(client):
+    responses.add(responses.GET, COLLECTIONS_URL, COLLECTIONS_RESPONSE, status=200,
+                  content_type='application/json')
+
+    api_root = ApiRoot(API_ROOT_URL, client=client)
+
+    assert api_root._loaded_collections is False
+    assert len(api_root.collections) == 2
+    assert api_root._loaded_collections is True
+
+    coll = api_root.collections[0]
+    # A collection populated from an API Root is automatically loaded
+    assert coll._loaded is True
+    assert coll.id == '91a7b528-80eb-42ed-a74d-c6fbd5a26116'
+    assert coll.url == COLLECTION_URL
+    assert coll.title == "High Value Indicator Collection"
+    assert coll.description == "This data collection is for collecting high value IOCs"
+    assert coll.can_read is True
+    assert coll.can_write is False
+    assert coll.media_types == [MEDIA_TYPE_STIX_V20]
+
+
+@responses.activate
+def test_collection(client):
+    responses.add(responses.GET, COLLECTION_URL, COLLECTION_RESPONSE,
+                  status=200, content_type='application/json')
+
+    coll = Collection(COLLECTION_URL, client=client)
+
+    assert coll._loaded is False
+    assert coll.id == '91a7b528-80eb-42ed-a74d-c6fbd5a26116'
+    assert coll._loaded is True
+    assert coll.url == COLLECTION_URL
+    assert coll.title == "High Value Indicator Collection"
+    assert coll.description == "This data collection is for collecting high value IOCs"
+    assert coll.can_read is True
+    assert coll.can_write is False
+    assert coll.media_types == [MEDIA_TYPE_STIX_V20]
