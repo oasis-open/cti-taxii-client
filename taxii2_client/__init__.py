28f1176741c2428559b9fb1bb2149cf669784f96
Michael Chisholm <chisholm@mitre.org>
2017-08-13 21:18:16 -0400

    Updates/refinements/improvements (hopefully) I made.
    - Added and used some more specific exception classes, instead of
      using ValueError everywhere
    
    - Renamed TAXII2Client to _HTTPConnection.  It's package-private,
      because I didn't think it needed to be user-visible.  The
      model classes now accept user/password directly (which will
      be used to create a new connection).
    
    - Renamed ServerDiscovery to Server
    
    - Refined how connections are shared among the components:
      * ServerDiscovery no longer shares connections with ApiRoot
        and descendant objects.  It returns ApiRoots with separate
        connections.
      * ApiRoots will share their connections with descendant
        collections.
      * Any of the classes may be manually instantiated so that they
        have their own connection, unshared with anyone else.
    
    - A few things were common among all model classes, so I factored
      them into a base class.  This includes some initialization,
      close(), and I added support to all model classes for use as
      context managers, which is in the base class.
    
    - Collection.add_objects() now supports blocking the caller until
      the addition is complete.  It's done by polling the service;
      there are additional parameters to control poll interval and
      timeout.  You can also elect to return immediately and not
      block.
    
    - get_collection_by_id() is now a function, instead of a method of
      ApiRoot.  I thought that since it didn't need access to any
      internals of ApiRoot, that perhaps it shouldn't be a member of it.
    
    - canonicalize_url() now ensures a trailing slash occurs in URLs,
      instead of ensuring it doesn't occur.  It also does a parse/
      reassemble cycle as the canonicalization step.
    
    - Removed the NO_PROXY setting
    
    - Fleshed out several docstrings.
    
    - Fixed up the unit tests so they pass
      * removed the client() fixture since you don't directly instantiate
        clients (now _HTTPConnections) anymore.

--- taxii2_client/__init__.py
+++ taxii2_client/__init__.py
@@ -1,11 +1,56 @@
-import os
-
 import requests
-
-os.environ["NO_PROXY"] = "localhost"
+import six.moves.urllib.parse as urlparse
+import time
 
 MEDIA_TYPE_STIX_V20 = "application/vnd.oasis.stix+json; version=2.0"
 MEDIA_TYPE_TAXII_V20 = "application/vnd.oasis.taxii+json; version=2.0"
+
+
+class TAXIIServiceException(Exception):
+    """Base class for exceptions raised by this library."""
+    def __init__(self, *args):
+        super(TAXIIServiceException, self).__init__(args)
+
+
+class InvalidArgumentsError(TAXIIServiceException):
+    """Invalid arguments were passed to a method."""
+    def __init__(self, msg):
+        super(InvalidArgumentsError, self).__init__(msg)
+
+
+class AccessError(TAXIIServiceException):
+    """Attempt was made to read/write to a collection when the collection
+    doesn't allow that operation."""
+    def __init__(self, msg):
+        super(AccessError, self).__init__(msg)
+
+
+class _TAXIIEndpoint(object):
+    """Contains some data and functionality common to all TAXII endpoint
+    classes: a URL, connection, and ability to close the connection.  It also
+    yields support in subclasses for use as contextmanagers, to ensure
+    resources are released.
+    """
+    def __init__(self, url, user=None, password=None, conn=None):
+        if conn and (user or password):
+            raise InvalidArgumentsError("A connection and user/password may"
+                                        " not both be provided.")
+        elif conn:
+            self._conn = conn
+        else:
+            self._conn = _HTTPConnection(user, password)
+
+        self.url = url
+
+    def close(self):
+        self._conn.close()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close()
+        return False
 
 
 # TODO: Should this object allow refreshing itself (have its own URL?)
@@ -19,8 +64,12 @@ class Status(object):
         self.failure_count = failure_count
         self.pending_count = pending_count
 
+    def __nonzero__(self):
+        return self.status == u"complete"
+    __bool__ = __nonzero__
 
-class Collection(object):
+
+class Collection(_TAXIIEndpoint):
     """Information about a TAXII Collection.
 
     This class represents the ``Get a Collection`` endpoint (section 5.2), and
@@ -32,12 +81,30 @@ class Collection(object):
         - ``Add Objects`` (section 5.4)
         - ``Get an Object`` (section 5.5)
         - ``Get Object Manifests`` (section 5.6)
+
+    As obtained from an ApiRoot, an instance of this class shares connection(s)
+    with all other collections obtained from the same ApiRoot, as well as the
+    ApiRoot instance itself.  Closing one will close them all.  If this is
+    undesirable, you may manually create Collection instances.
     """
 
-    def __init__(self, url, client=None, **kwargs):
-        self.url = url
+    def __init__(self, url, user=None, password=None, conn=None, **kwargs):
+        """
+        Initialize a new Collection.  Either user/password or conn may be
+        given, but not both.  The latter is intended for internal use, when
+        sharing connection pools with an ApiRoot, mocking a connection for
+        testing, etc.  Users should use user/password (if required) which will
+        create a new connection.
 
-        self._client = client
+        :param url: A TAXII endpoint for a collection
+        :param user: User name for authentication (optional)
+        :param password: Password for authentication (optional)
+        :param conn: A _HTTPConnection to reuse (optional)
+        :param kwargs: Collection metadata, if known in advance (optional)
+        """
+
+        super(Collection, self).__init__(url, user, password, conn)
+
         self._loaded = False
 
         # Since the API Root "Get Collections" endpoint returns information on
@@ -95,7 +162,7 @@ class Collection(object):
             self.refresh()
 
     def refresh(self):
-        response = self._client.get(self.url, accept=MEDIA_TYPE_TAXII_V20)
+        response = self._conn.get(self.url, accept=MEDIA_TYPE_TAXII_V20)
         self._populate_fields(**response)
 
         self._loaded = True
@@ -104,54 +171,103 @@ class Collection(object):
         """Implement the ``Get Objects`` endpoint (section 5.3)"""
         # TODO: add filters
         if not self.can_read:
-            raise ValueError("Collection {} does not allow reading".format(self.url))
+            raise AccessError("Collection {} does not allow reading".format(
+                self.url))
         url = self.url + "objects/"
-        return self._client.get(url, accept=MEDIA_TYPE_STIX_V20)
+        return self._conn.get(url, accept=MEDIA_TYPE_STIX_V20)
 
     # TODO: update this function
     def get_object(self, obj_id):
         if not self.can_read:
-            raise ValueError("Collection %s of %s does not allow reading" % (self.id_, self.api_root.uri))
-        return self.taxii_client.send_request("get",
-                                              "/".join([self.api_root.url, "collections", self.id_, "objects", obj_id]),
-                                              {"Accept": MEDIA_TYPE_STIX_V20})
+            raise AccessError("Collection %s of %s does not allow reading" %
+                             (self.id_, self.api_root.uri))
+        return self._conn.get("/".join([self.api_root.url, "collections", self.id_, "objects", obj_id]),
+                              MEDIA_TYPE_STIX_V20)
 
-    # TODO: update this function
-    def add_objects(self, bundle):
+    def add_objects(self, bundle, wait_for_completion=True, poll_interval=1,
+                    timeout=60):
+        """
+        Add objects to the collection.  This may be performed either
+        synchronously or asynchronously.  To add asynchronously, set
+        wait_for_completion to False.  If False, the latter two args are
+        unused.  If the caller wishes to monitor the status of the addition,
+        it may do so in its own way.  To add synchronously, set
+        wait_for_completion to True, and optionally set the poll and timeout
+        intervals.  After initiating the addition, the caller will block,
+        and the TAXII "status" service will be polled until the timeout
+        expires, or the operation completes.
+
+        :param bundle: A STIX bundle with the objects to add (JSON as it would
+            be parsed into native Python).
+        :param wait_for_completion: Whether to wait for the add operation to
+            complete before returning
+        :param poll_interval: If waiting for completion, how often to poll
+            the status service (seconds)
+        :param timeout: If waiting for completion, how long to poll until
+            giving up (seconds).  Use <= 0 to wait forever.
+        :return: If wait_for_completion is False, a Status object corresponding
+            to the initial status data returned from the service, is returned.
+            The status may not yet be complete at this point.  If
+            wait_for_completion is True, a Status object corresponding to the
+            completed operation is returned if it didn't time out; otherwise
+            a Status object corresponding to the most recent data obtained
+            before the timeout, is returned.
+        """
         if not self.can_write:
-            raise ValueError("Collection %s of %s does not allow writing" % (self.id_, self.api_root.uri))
-        info = self.taxii_client.send_request("post",
-                                              "/".join([self.api_root.url, "collections", self.id_, "objects"])+"/",
-                                              {"Accept": MEDIA_TYPE_TAXII_V20,
-                                               "Content-Type": MEDIA_TYPE_STIX_V20},
-                                              json=bundle)
-        return Status(**info)
+            raise AccessError(u"Collection %s of %s does not allow writing" %
+                             (self.id_, self.api_root.uri))
+
+        add_url = urlparse.urljoin(self.url, "objects/")
+
+        status_json = self._conn.post(add_url,
+            headers={u"Accept": MEDIA_TYPE_TAXII_V20,
+                     u"Content-Type": MEDIA_TYPE_STIX_V20},
+            json=bundle)
+
+        if not wait_for_completion or status_json[u"status"] == u"complete":
+            return Status(**status_json)
+
+        status_url = urlparse.urljoin(self.url, u"../../status/{}".format(
+            status_json[u"id"]))
+
+        start_time = time.time()
+        elapsed = 0
+        while status_json[u"status"] != u"complete" and \
+                (timeout <= 0 or elapsed < timeout):
+            time.sleep(poll_interval)
+            status_json = self._conn.get(status_url, MEDIA_TYPE_TAXII_V20)
+            elapsed = time.time() - start_time
+
+        return Status(**status_json)
 
     # TODO: update this function
     def get_manifest(self, filters=None):
-        return self.taxii_client.send_request("get",
-                                              "/".join([self.api_root.url, "collections", self.id_, "manifest"]),
-                                              {"Accept": MEDIA_TYPE_TAXII_V20})
+        return self._conn.get("/".join([self.api_root.url, "collections", self.id_, "manifest"]),
+                              MEDIA_TYPE_TAXII_V20)
 
 
-class ApiRoot(object):
+class ApiRoot(_TAXIIEndpoint):
     """Information about a TAXII API Root.
 
     This class corresponds to the ``Get API Root Information`` (section 4.2) and
     ``Get Collections`` (section 5.1) endpoints, and contains the information
     found in the corresponding ``API Root Resource`` (section 4.2.1) and
     ``Collections Resource`` (section 5.1.1).
+
+    As obtained from a Server, each ApiRoot instance gets its own connection
+    pool(s).  Collections returned by instances of this class share the same
+    pools as the instance, so closing one closes all.  Also, the same
+    username/password is used to connect to them, as was used for this ApiRoot.
+    If either of these is undesirable, Collection instances may be created
+    manually.
     """
 
-    def __init__(self, url, client=None):
-        self.url = url
+    def __init__(self, url, user=None, password=None, conn=None):
 
-        self._client = client
+        super(ApiRoot, self).__init__(url, user, password, conn)
+
         self._loaded_collections = False
         self._loaded_information = False
-
-        # TODO: Consider adding a `name` @property: the last URL path component
-        # self.name = name
 
     @property
     def collections(self):
@@ -193,7 +309,7 @@ class ApiRoot(object):
 
         This invokes the ``Get API Root Information`` endpoint.
         """
-        response = self._client.get(self.url, accept=MEDIA_TYPE_TAXII_V20)
+        response = self._conn.get(self.url, accept=MEDIA_TYPE_TAXII_V20)
 
         self._title = response['title']
         self._description = response['description']
@@ -208,42 +324,52 @@ class ApiRoot(object):
         This invokes the ``Get Collections`` endpoint.
         """
         url = self.url + 'collections/'
-        response = self._client.get(url, accept=MEDIA_TYPE_TAXII_V20)
+        response = self._conn.get(url, accept=MEDIA_TYPE_TAXII_V20)
 
         self._collections = []
         print(response)
         for item in response['collections']:
             collection_url = url + item['id'] + "/"
-            collection = Collection(collection_url, client=self._client, **item)
+            collection = Collection(collection_url, conn=self._conn, **item)
             self._collections.append(collection)
 
         self._loaded_collections = True
 
     # TODO: update this function
-    def get_collection(self, id_, refresh=False):
-        for c in self.get_collections(refresh):
-            if c.id_ == id_:
-                return c
-
-    # TODO: update this function
     def get_status(self, id_):
-        info = self.taxii_client.send_request("get",
-                                              "/".join([self.url, "status", id_]),
-                                              {"Accept": MEDIA_TYPE_TAXII_V20})
+        info = self._conn.get("/".join([self.url, "status", id_]),
+                              {"Accept": MEDIA_TYPE_TAXII_V20})
         return Status(**info)
 
 
-class ServerDiscovery(object):
+class Server(_TAXIIEndpoint):
     """Information about a server hosting a Discovery service.
 
     This class corresponds to the Server Discovery endpoint (section 4.1) and
     the Discovery Resource returned from that endpoint (section 4.1.1).
+
+    ApiRoot instances obtained from an instance of this class are
+    created with the same username/password as was used in this instance.  If
+    that's incorrect, an ApiRoot instance may be created directly with the
+    desired username and password.  Also, they use separate connection pools
+    so that they can be independent: closing one won't close others, and
+    closing this server object won't close any of the ApiRoot objects (which
+    may refer to different hosts than was used for discovery).
     """
 
-    def __init__(self, hostname, client=None):
-        self.hostname = hostname
+    def __init__(self, url, user=None, password=None, conn=None):
+        """
+        :param url: URL of a TAXII server discovery endpoint
+        :param user: username for authentication (optional)
+        :param password: password for authentication (optional)
+        :param conn: A connection object, as an alternative to providing
+            username/password
+        """
 
-        self._client = client
+        super(Server, self).__init__(url, user, password, conn)
+
+        self._user = user
+        self._password = password
         self._loaded = False
 
     @property
@@ -276,15 +402,14 @@ class ServerDiscovery(object):
             self.refresh()
 
     def refresh(self):
-        # TODO: ensure client exists
-        url = "https://{}/taxii/".format(self.hostname)
-        response = self._client.get(url, accept=MEDIA_TYPE_TAXII_V20)
+        response = self._conn.get(self.url, accept=MEDIA_TYPE_TAXII_V20)
 
         self._title = response['title']
         self._description = response.get('description')
         self._contact = response.get('contact')
         roots = response.get('api_roots', [])
-        self._api_roots = [ApiRoot(url, client=self._client) for url in roots]
+        self._api_roots = [ApiRoot(url, self._user, self._password)
+                           for url in roots]
         # If 'default' is one of the existing API Roots, reuse that object
         # rather than creating a duplicate. The TAXII 2.0 spec says that the
         # `default` API Root MUST be an item in `api_roots`.
@@ -294,11 +419,22 @@ class ServerDiscovery(object):
         self._loaded = True
 
 
-class TAXII2Client(object):
+class _HTTPConnection(object):
+    """This library uses the "requests" library, which presents a convenience
+    API which hides many network details like actual connection objects.  So
+    this class doesn't represent a traditional "connection" either.  It's a
+    sort of approximation: sets of connections (or connection pools) and common
+    metadata for a particular server interaction.  You can send requests to
+    any hosts via the same instance; hosts/ports are not checked and new
+    connection pools pop into existence as needed, but all connections are
+    closed when the close() method is called.  So this is intended to be used
+    for an independent self-contained interaction.
+    """
 
-    def __init__(self, user, password):
+    def __init__(self, user=None, password=None):
         self.session = requests.Session()
-        self.session.auth = requests.auth.HTTPBasicAuth(user, password)
+        if user and password:
+            self.session.auth = requests.auth.HTTPBasicAuth(user, password)
 
     def get(self, url, accept):
         """Perform an HTTP GET, using the saved requests.Session and auth info.
@@ -314,27 +450,40 @@ class TAXII2Client(object):
         }
         resp = self.session.get(url, headers=headers)
 
+        resp.raise_for_status()
+
         content_type = resp.headers['Content-Type']
         if content_type != accept:
             msg = "Unexpected Response Content-Type: {}"
-            raise ValueError(msg.format(content_type))
-        resp.raise_for_status()
+            raise TAXIIServiceException(msg.format(content_type))
+
         return resp.json()
 
     def post(self, url, headers=None, params=None, json=None):
+        """Send a JSON POST request with the given request headers, additional
+        URL query parameters, and the given JSON in the request body.  The
+        extra query parameters are merged with any which already exist in the
+        URL.
+        """
         resp = self.session.post(url, headers=headers, params=params, json=json)
         resp.raise_for_status()
         return resp.json()
 
+    def close(self):
+        """Closes connections.  This object is no longer usable."""
+        self.session.close()
 
-# TODO: are these needed?
-def canonicalize_api_root_url(api_root_url):
-    if api_root_url.endswith("/"):
-        api_root_url = api_root_url[:-1]
+
+def get_collection_by_id(api_root, id_):
+    for c in api_root.collections:
+        if c.id == id_:
+            return c
+
+    return None
+
+
+def canonicalize_url(api_root_url):
+    api_root_url = urlparse.urlsplit(api_root_url).geturl()
+    if not api_root_url.endswith("/"):
+        api_root_url += "/"
     return api_root_url
-
-
-def get_api_root_name(api_root_url):
-    if api_root_url.endswith("/"):
-        api_root_url = api_root_url[:-1]
-    return api_root_url.split('/')[-1]
