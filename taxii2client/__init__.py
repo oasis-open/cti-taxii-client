0fbae5e895719e1cc811058f4ab0f2ca8dacfc21
Emmanuelle Vargas-Gonzalez <emmanuelle@mitre.org>
2020-03-27 19:49:20 -0400

    move media_types and user_agent to top of module

--- taxii2client/__init__.py
+++ taxii2client/__init__.py
@@ -1,1 +1,7 @@
 """Python TAXII 2.1 APIs"""
+from .version import __version__
+
+DEFAULT_USER_AGENT = "taxii2-client/" + __version__
+MEDIA_TYPE_STIX_V20 = "application/vnd.oasis.stix+json; version=2.0"
+MEDIA_TYPE_TAXII_V20 = "application/vnd.oasis.taxii+json; version=2.0"
+MEDIA_TYPE_TAXII_V21 = "application/taxii+json; version=2.1"
