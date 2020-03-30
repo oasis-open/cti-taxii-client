cb2b02fd07b54d739aa77264661b5c89eb07e6c8
Emmanuelle Vargas-Gonzalez <emmanuelle@mitre.org>
2020-03-30 12:33:01 -0400

    packaging update

--- setup.py
+++ setup.py
@@ -1,24 +1,37 @@
 #!/usr/bin/env python
 from codecs import open
+import os
 
 from setuptools import find_packages, setup
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+VERSION_FILE = os.path.join(BASE_DIR, 'taxii2client', 'version.py')
+
+
+def get_version():
+    with open(VERSION_FILE) as f:
+        for line in f.readlines():
+            if line.startswith('__version__'):
+                version = line.split()[-1].strip('"')
+                return version
+        raise AttributeError("Package does not have a __version__")
+
 
 def get_long_description():
-    with open('README.rst')as f:
+    with open('README.rst') as f:
         return f.read()
 
 
 setup(
     name='taxii2-client',
-    version='0.5.0',
+    version=get_version(),
     description='TAXII 2 Client Library',
     long_description=get_long_description(),
-    url='https://github.com/oasis-open/cti-taxii-client',
+    url='https://oasis-open.github.io/cti-documentation/',
     author='OASIS Cyber Threat Intelligence Technical Committee',
     author_email='cti-users@lists.oasis-open.org',
-    maintainer='Greg Back',
-    maintainer_email='gback@mitre.org',
+    maintainer='Emmanuelle Vargas-Gonzalez',
+    maintainer_email='emmanuelle@mitre.org',
     license='BSD',
     classifiers=[
         'Development Status :: 3 - Alpha',
@@ -33,6 +46,7 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
     keywords="taxii taxii2 client json cti cyber threat intelligence",
     packages=find_packages(exclude=['*.test']),
@@ -53,5 +67,10 @@ setup(
             'sphinx',
             'sphinx-prompt',
         ]
-    }
+    },
+    project_urls={
+        'Documentation': 'https://taxii2client.readthedocs.io/',
+        'Source Code': 'https://github.com/oasis-open/cti-taxii-client/',
+        'Bug Tracker': 'https://github.com/oasis-open/cti-taxii-client/issues/',
+    },
 )
