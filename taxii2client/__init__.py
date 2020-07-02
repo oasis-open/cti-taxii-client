47bce30b43809f48c1788727ddebe5d646c59f6e
Emmanuelle Vargas-Gonzalez <emmanuelle@mitre.org>
2020-07-02 18:21:05 -0400

    Bump version: 2.1.0 → 2.2.0

--- taxii2client/__init__.py
+++ taxii2client/__init__.py
@@ -2,7 +2,7 @@
 
 # flake8: noqa
 
-DEFAULT_USER_AGENT = "taxii2-client/2.1.0"
+DEFAULT_USER_AGENT = "taxii2-client/2.2.0"
 MEDIA_TYPE_STIX_V20 = "application/vnd.oasis.stix+json; version=2.0"
 MEDIA_TYPE_TAXII_V20 = "application/vnd.oasis.taxii+json; version=2.0"
 MEDIA_TYPE_TAXII_V21 = "application/taxii+json; version=2.1"
