f7daa83c18f6346d1e4f062dee571d5fca307300
Emmanuelle Vargas-Gonzalez <emmanuelle@mitre.org>
2020-09-10 17:52:59 -0400

    Bump version: 2.2.1 → 2.2.2

--- taxii2client/__init__.py
+++ taxii2client/__init__.py
@@ -15,7 +15,7 @@ log.propagate = False
 log.addHandler(ch)
 
 
-DEFAULT_USER_AGENT = "taxii2-client/2.2.1"
+DEFAULT_USER_AGENT = "taxii2-client/2.2.2"
 MEDIA_TYPE_STIX_V20 = "application/vnd.oasis.stix+json; version=2.0"
 MEDIA_TYPE_TAXII_V20 = "application/vnd.oasis.taxii+json; version=2.0"
 MEDIA_TYPE_TAXII_V21 = "application/taxii+json; version=2.1"
